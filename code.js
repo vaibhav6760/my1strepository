fac67a12cc7a4098b8367dd331ca36bd4e1021ef
vxibhxv304 <gangishettyvaibhav@gmail.com>
2025-06-25 15:13:38 +0530

    js

---
+++ code.js
@@ -0,0 +1,25 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "professorBot";
+
+let botLocation = "Codeville";
+
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " was created in just 10 days!";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is primarily used for web development.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " can run both on the client and the server.";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
