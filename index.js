cc74790caef0b075ba7174e2a9a45af78d1c6f4d
vxibhxv304 <gangishettyvaibhav@gmail.com>
2025-07-02 15:38:37 +0530

    13

--- index.js
+++ index.js
@@ -123,3 +123,38 @@ getData(101)
     console.log("Second fetch:", result);
     return getData(103)
 })
+
+
+function getData(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Fetching data for id:", id);
+            resolve( id);
+        }, 1000);
+    });
+}
+
+async function fetchData() {
+    try {
+        const data1 = await getData(101);
+        console.log("First fetch:", data1);
+        const data2 = await getData(102);
+        console.log("Second fetch:", data2);
+        const data3 = await getData(103);
+        console.log("Third fetch:", data3);
+    } catch (error) {
+        console.error("Error occured:", error);
+    }
+}
+fetchData();
+
+async function getnextData() {
+    console.log("getting data 1....");
+    await getData(1);
+     console.log("getting data 2....");
+    await getData(2);
+     console.log("getting data 3....");
+    await getData(3);
+}
+getnextData();
+
